d66f8dd7d3aa9f99069f4a4c5da5bd3d9b82d328
Sreelal TS <heysreelal@gmail.com>
2023-10-28 14:26:25 +0530

    🇺🇳 Individual countries

--- lib/src/country.dart
+++ lib/src/country.dart
@@ -1,51 +1,776 @@
 part of phonecodes;
 
 /// This class represents a Country with its name, code, dialCode and flag.
-final class Country {
-  /// The name of the country
+enum Country {
+  /// Afghanistan
+  afghanistan._("Afghanistan", "AF", "+93", "🇦🇫"),
+
+  /// Albania
+  albania._("Albania", "AL", "+355", "🇦🇱"),
+
+  /// Algeria
+  algeria._("Algeria", "DZ", "+213", "🇩🇿"),
+
+  /// American Samoa
+  americanSamoa._("American Samoa", "AS", "+1684", "🇦🇸"),
+
+  /// Andorra
+  andorra._("Andorra", "AD", "+376", "🇦🇩"),
+
+  /// Angola
+  angola._("Angola", "AO", "+244", "🇦🇴"),
+
+  /// Anguilla
+  anguilla._("Anguilla", "AI", "+1264", "🇦🇮"),
+
+  /// Antigua and Barbuda
+  antiguaAndBarbuda._("Antigua and Barbuda", "AG", "+1268", "🇦🇬"),
+
+  /// Argentina
+  argentina._("Argentina", "AR", "+54", "🇦🇷"),
+
+  /// Armenia
+  armenia._("Armenia", "AM", "+374", "🇦🇲"),
+
+  /// Aruba
+  aruba._("Aruba", "AW", "+297", "🇦🇼"),
+
+  /// Australia
+  australia._("Australia", "AU", "+61", "🇦🇺"),
+
+  /// Austria
+  austria._("Austria", "AT", "+43", "🇦🇹"),
+
+  /// Azerbaijan
+  azerbaijan._("Azerbaijan", "AZ", "+994", "🇦🇿"),
+
+  /// Bahamas
+  bahamas._("Bahamas", "BS", "+1242", "🇧🇸"),
+
+  /// Bahrain
+  bahrain._("Bahrain", "BH", "+973", "🇧🇭"),
+
+  /// Bangladesh
+  bangladesh._("Bangladesh", "BD", "+880", "🇧🇩"),
+
+  /// Barbados
+  barbados._("Barbados", "BB", "+1246", "🇧🇧"),
+
+  /// Belarus
+  belarus._("Belarus", "BY", "+375", "🇧🇾"),
+
+  /// Belgium
+  belgium._("Belgium", "BE", "+32", "🇧🇪"),
+
+  /// Belize
+  belize._("Belize", "BZ", "+501", "🇧🇿"),
+
+  /// Benin
+  benin._("Benin", "BJ", "+229", "🇧🇯"),
+
+  /// Bermuda
+  bermuda._("Bermuda", "BM", "+1441", "🇧🇲"),
+
+  /// Bhutan
+  bhutan._("Bhutan", "BT", "+975", "🇧🇹"),
+
+  /// Bolivia
+  bolivia._("Bolivia", "BO", "+591", "🇧🇴"),
+
+  /// Bosnia and Herzegovina
+  bosniaAndHerzegovina._("Bosnia and Herzegovina", "BA", "+387", "🇧🇦"),
+
+  /// Botswana
+  botswana._("Botswana", "BW", "+267", "🇧🇼"),
+
+  /// Brazil
+  brazil._("Brazil", "BR", "+55", "🇧🇷"),
+
+  /// British Indian Ocean Territory
+  britishIndianOceanTerritory._(
+      "British Indian Ocean Territory", "IO", "+246", "🇮🇴"),
+
+  /// British Virgin Islands
+  britishVirginIslands._("British Virgin Islands", "VG", "+1284", "🇻🇬"),
+
+  /// Brunei
+  brunei._("Brunei", "BN", "+673", "🇧🇳"),
+
+  /// Bulgaria
+  bulgaria._("Bulgaria", "BG", "+359", "🇧🇬"),
+
+  /// Burkina Faso
+  burkinaFaso._("Burkina Faso", "BF", "+226", "🇧🇫"),
+
+  /// Burundi
+  burundi._("Burundi", "BI", "+257", "🇧🇮"),
+
+  /// Cambodia
+  cambodia._("Cambodia", "KH", "+855", "🇰🇭"),
+
+  /// Cameroon
+  cameroon._("Cameroon", "CM", "+237", "🇨🇲"),
+
+  /// Canada
+  canada._("Canada", "CA", "+1", "🇨🇦"),
+
+  /// Cape Verde
+  capeVerde._("Cape Verde", "CV", "+238", "🇨🇻"),
+
+  /// Cayman Islands
+  caymanIslands._("Cayman Islands", "KY", "+1345", "🇰🇾"),
+
+  /// Central African Republic
+  centralAfricanRepublic._("Central African Republic", "CF", "+236", "🇨🇫"),
+
+  /// Chad
+  chad._("Chad", "TD", "+235", "🇹🇩"),
+
+  /// Chile
+  chile._("Chile", "CL", "+56", "🇨🇱"),
+
+  /// China
+  china._("China", "CN", "+86", "🇨🇳"),
+
+  /// Christmas Island
+  christmasIsland._("Christmas Island", "CX", "+61", "🇨🇽"),
+
+  /// Cocos Islands
+  cocosIslands._("Cocos Islands", "CC", "+61", "🇨🇨"),
+
+  /// Colombia
+  colombia._("Colombia", "CO", "+57", "🇨🇴"),
+
+  /// Comoros
+  comoros._("Comoros", "KM", "+269", "🇰🇲"),
+
+  /// Cook Islands
+  cookIslands._("Cook Islands", "CK", "+682", "🇨🇰"),
+
+  /// Costa Rica
+  costaRica._("Costa Rica", "CR", "+506", "🇨🇷"),
+
+  /// Croatia
+  croatia._("Croatia", "HR", "+385", "🇭🇷"),
+
+  /// Cuba
+  cuba._("Cuba", "CU", "+53", "🇨🇺"),
+
+  /// Curacao
+  curacao._("Curacao", "CW", "+599", "🇨🇼"),
+
+  /// Cyprus
+  cyprus._("Cyprus", "CY", "+357", "🇨🇾"),
+
+  /// Czech Republic
+  czechRepublic._("Czech Republic", "CZ", "+420", "🇨🇿"),
+
+  /// Democratic Republic of the Congo
+  democraticRepublicOfTheCongo._(
+      "Democratic Republic of the Congo", "CD", "+243", "🇨🇩"),
+
+  /// Denmark
+  denmark._("Denmark", "DK", "+45", "🇩🇰"),
+
+  /// Djibouti
+  djibouti._("Djibouti", "DJ", "+253", "🇩🇯"),
+
+  /// Dominica
+  dominica._("Dominica", "DM", "+1767", "🇩🇲"),
+
+  /// Dominican Republic
+  dominicanRepublic._("Dominican Republic", "DO", "+1809", "🇩🇴"),
+
+  /// Dominican Republic
+  dominicanRepublic1829._("Dominican Republic", "DO", "+1829", "🇩🇴"),
+
+  /// Dominican Republic
+  dominicanRepublic1849._("Dominican Republic", "DO", "+1849", "🇩🇴"),
+
+  /// Ecuador
+  ecuador._("Ecuador", "EC", "+593", "🇪🇨"),
+
+  /// Egypt
+  egypt._("Egypt", "EG", "+20", "🇪🇬"),
+
+  /// El Salvador
+  elSalvador._("El Salvador", "SV", "+503", "🇸🇻"),
+
+  /// Equatorial Guinea
+  equatorialGuinea._("Equatorial Guinea", "GQ", "+240", "🇬🇶"),
+
+  /// Eritrea
+  eritrea._("Eritrea", "ER", "+291", "🇪🇷"),
+
+  /// Estonia
+  estonia._("Estonia", "EE", "+372", "🇪🇪"),
+
+  /// Ethiopia
+  ethiopia._("Ethiopia", "ET", "+251", "🇪🇹"),
+
+  /// Falkland Islands
+  falklandIslands._("Falkland Islands", "FK", "+500", "🇫🇰"),
+
+  /// Faroe Islands
+  faroeIslands._("Faroe Islands", "FO", "+298", "🇫🇴"),
+
+  /// Fiji
+  fiji._("Fiji", "FJ", "+679", "🇫🇯"),
+
+  /// Finland
+  finland._("Finland", "FI", "+358", "🇫🇮"),
+
+  /// France
+  france._("France", "FR", "+33", "🇫🇷"),
+
+  /// French Guiana
+  frenchGuiana._("French Guiana", "GF", "+594", "🇬🇫"),
+
+  /// French Polynesia
+  frenchPolynesia._("French Polynesia", "PF", "+689", "🇵🇫"),
+
+  /// Gabon
+  gabon._("Gabon", "GA", "+241", "🇬🇦"),
+
+  /// Gambia
+  gambia._("Gambia", "GM", "+220", "🇬🇲"),
+
+  /// Georgia
+  georgia._("Georgia", "GE", "+995", "🇬🇪"),
+
+  /// Germany
+  germany._("Germany", "DE", "+49", "🇩🇪"),
+
+  /// Ghana
+  ghana._("Ghana", "GH", "+233", "🇬🇭"),
+
+  /// Gibraltar
+  gibraltar._("Gibraltar", "GI", "+350", "🇬🇮"),
+
+  /// Global Mobile Satellite System
+  globalMobileSatelliteSystem._(
+      "Global Mobile Satellite System", "GMS", "+881", "🛰"),
+
+  /// Greece
+  greece._("Greece", "GR", "+30", "🇬🇷"),
+
+  /// Greenland
+  greenland._("Greenland", "GL", "+299", "🇬🇱"),
+
+  /// Grenada
+  grenada._("Grenada", "GD", "+1473", "🇬🇩"),
+
+  /// Guadeloupe
+  guadeloupe._("Guadeloupe", "GP", "+590", "🇬🇵"),
+
+  /// Guam
+  guam._("Guam", "GU", "+1671", "🇬🇺"),
+
+  /// Guatemala
+  guatemala._("Guatemala", "GT", "+502", "🇬🇹"),
+
+  /// Guernsey
+  guernsey._("Guernsey", "GG", "+44", "🇬🇬"),
+
+  /// Guinea
+  guinea._("Guinea", "GN", "+224", "🇬🇳"),
+
+  /// Guinea-Bissau
+  guineaBissau._("Guinea-Bissau", "GW", "+245", "🇬🇼"),
+
+  /// Guyana
+  guyana._("Guyana", "GY", "+592", "🇬🇾"),
+
+  /// Haiti
+  haiti._("Haiti", "HT", "+509", "🇭🇹"),
+
+  /// Honduras
+  honduras._("Honduras", "HN", "+504", "🇭🇳"),
+
+  /// Hong Kong
+  hongKong._("Hong Kong", "HK", "+852", "🇭🇰"),
+
+  /// Hungary
+  hungary._("Hungary", "HU", "+36", "🇭🇺"),
+
+  /// Iceland
+  iceland._("Iceland", "IS", "+354", "🇮🇸"),
+
+  /// India
+  india._("India", "IN", "+91", "🇮🇳"),
+
+  /// Indonesia
+  indonesia._("Indonesia", "ID", "+62", "🇮🇩"),
+
+  /// Iran
+  iran._("Iran", "IR", "+98", "🇮🇷"),
+
+  /// International Networks
+  internationalNetworks._("International Networks", "INT", "+882", "🌍"),
+
+  /// International Networks
+  internationalNetworks883._("International Networks", "INT", "+883", "🌍"),
+
+  /// Iraq
+  iraq._("Iraq", "IQ", "+964", "🇮🇶"),
+
+  /// Ireland
+  ireland._("Ireland", "IE", "+353", "🇮🇪"),
+
+  /// Israel
+  israel._("Israel", "IL", "+972", "🇮🇱"),
+
+  /// Italy
+  italy._("Italy", "IT", "39", "🇮🇹"),
+
+  /// Jamaica
+  jamaica._("Jamaica", "JM", "+1876", "🇯🇲"),
+
+  /// Japan
+  japan._("Japan", "JP", "+81", "🇯🇵"),
+
+  /// Jersey
+  jersey._("Jersey", "JE", "+44", "🇯🇪"),
+
+  /// Jordan
+  jordan._("Jordan", "JO", "+962", "🇯🇴"),
+
+  /// Kazakhstan
+  kazakhstan._("Kazakhstan", "KZ", "+7", "🇰🇿"),
+
+  /// Kenya
+  kenya._("Kenya", "KE", "+254", "🇰🇪"),
+
+  /// Kiribati
+  kiribati._("Kiribati", "KI", "+686", "🇰🇮"),
+
+  /// Kosovo
+  kosovo._("Kosovo", "XK", "+383", "🇽🇰"),
+
+  /// Kuwait
+  kuwait._("Kuwait", "KW", "+965", "🇰🇼"),
+
+  /// Kyrgyzstan
+  kyrgyzstan._("Kyrgyzstan", "KG", "+996", "🇰🇬"),
+
+  /// Laos
+  laos._("Laos", "LA", "+856", "🇱🇦"),
+
+  /// Latvia
+  latvia._("Latvia", "LV", "+371", "🇱🇻"),
+
+  /// Lebanon
+  lebanon._("Lebanon", "LB", "+961", "🇱🇧"),
+
+  /// Lesotho
+  lesotho._("Lesotho", "LS", "+266", "🇱🇸"),
+
+  /// Liberia
+  liberia._("Liberia", "LR", "+231", "🇱🇷"),
+
+  /// Libya
+  libya._("Libya", "LY", "+218", "🇱🇾"),
+
+  /// Liechtenstein
+  liechtenstein._("Liechtenstein", "LI", "+423", "🇱🇮"),
+
+  /// Lithuania
+  lithuania._("Lithuania", "LT", "+370", "🇱🇹"),
+
+  /// Luxembourg
+  luxembourg._("Luxembourg", "LU", "+352", "🇱🇺"),
+
+  /// Macau
+  macau._("Macau", "MO", "+853", "🇲🇴"),
+
+  /// Macedonia
+  macedonia._("Macedonia", "MK", "+389", "🇲🇰"),
+
+  /// Madagascar
+  madagascar._("Madagascar", "MG", "+261", "🇲🇬"),
+
+  /// Malawi
+  malawi._("Malawi", "MW", "+265", "🇲🇼"),
+
+  /// Malaysia
+  malaysia._("Malaysia", "MY", "+60", "🇲🇾"),
+
+  /// Maldives
+  maldives._("Maldives", "MV", "+960", "🇲🇻"),
+
+  /// Mali
+  mali._("Mali", "ML", "+223", "🇲🇱"),
+
+  /// Malta
+  malta._("Malta", "MT", "+356", "🇲🇹"),
+
+  /// Marshall Islands
+  marshallIslands._("Marshall Islands", "MH", "+692", "🇲🇭"),
+
+  /// Martinique
+  martinique._("Martinique", "MQ", "+596", "🇲🇶"),
+
+  /// Mauritania
+  mauritania._("Mauritania", "MR", "+222", "🇲🇷"),
+
+  /// Mauritius
+  mauritius._("Mauritius", "MU", "+230", "🇲🇺"),
+
+  /// Mayotte
+  mayotte._("Mayotte", "YT", "+262", "🇾🇹"),
+
+  /// Mexico
+  mexico._("Mexico", "MX", "+52", "🇲🇽"),
+
+  /// Micronesia
+  micronesia._("Micronesia", "FM", "+691", "🇫🇲"),
+
+  /// Moldova
+  moldova._("Moldova", "MD", "+373", "🇲🇩"),
+
+  /// Monaco
+  monaco._("Monaco", "MC", "+377", "🇲🇨"),
+
+  /// Mongolia
+  mongolia._("Mongolia", "MN", "+976", "🇲🇳"),
+
+  /// Montenegro
+  montenegro._("Montenegro", "ME", "+382", "🇲🇪"),
+
+  /// Montserrat
+  montserrat._("Montserrat", "MS", "+1664", "🇲🇸"),
+
+  /// Morocco
+  morocco._("Morocco", "MA", "+212", "🇲🇦"),
+
+  /// Mozambique
+  mozambique._("Mozambique", "MZ", "+258", "🇲🇿"),
+
+  /// Myanmar
+  myanmar._("Myanmar", "MM", "+95", "🇲🇲"),
+
+  /// Namibia
+  namibia._("Namibia", "NA", "+264", "🇳🇦"),
+
+  /// Nauru
+  nauru._("Nauru", "NR", "+674", "🇳🇷"),
+
+  /// Nepal
+  nepal._("Nepal", "NP", "+977", "🇳🇵"),
+
+  /// Netherlands
+  netherlands._("Netherlands", "NL", "+31", "🇳🇱"),
+
+  /// New Caledonia
+  newCaledonia._("New Caledonia", "NC", "+687", "🇳🇨"),
+
+  /// New Zealand
+  newZealand._("New Zealand", "NZ", "+64", "🇳🇿"),
+
+  /// Nicaragua
+  nicaragua._("Nicaragua", "NI", "+505", "🇳🇮"),
+
+  /// Niger
+  niger._("Niger", "NE", "+227", "🇳🇪"),
+
+  /// Nigeria
+  nigeria._("Nigeria", "NG", "+234", "🇳🇬"),
+
+  /// Niue
+  niue._("Niue", "NU", "+683", "🇳🇺"),
+
+  /// Norfolk Island
+  norfolkIsland._("Norfolk Island", "NF", "+672", "🇳🇫"),
+
+  /// North Korea
+  northKorea._("North Korea", "KP", "+850", "🇰🇵"),
+
+  /// Northern Mariana Islands
+  northernMarianaIslands._("Northern Mariana Islands", "MP", "+1670", "🇲🇵"),
+
+  /// Norway
+  norway._("Norway", "NO", "+47", "🇳🇴"),
+
+  /// Oman
+  oman._("Oman", "OM", "+968", "🇴🇲"),
+
+  /// Pakistan
+  pakistan._("Pakistan", "PK", "+92", "🇵🇰"),
+
+  /// Palau
+  palau._("Palau", "PW", "+680", "🇵🇼"),
+
+  /// Palastinian Territories
+  palastinianTerritories._("Palastinian Territories", "PS", "+970", "🇵🇸"),
+
+  /// Panama
+  panama._("Panama", "PA", "+507", "🇵🇦"),
+
+  /// Papua New Guinea
+  papuaNewGuinea._("Papua New Guinea", "PG", "+675", "🇵🇬"),
+
+  /// Paraguay
+  paraguay._("Paraguay", "PY", "+595", "🇵🇾"),
+
+  /// Peru
+  peru._("Peru", "PE", "+51", "🇵🇪"),
+
+  /// Philippines
+  philippines._("Philippines", "PH", "+63", "🇵🇭"),
+
+  /// Pitcairn Islands
+  pitcairnIslands._("Pitcairn Islands", "PN", "+64", "🇵🇳"),
+
+  /// Poland
+  poland._("Poland", "PL", "+48", "🇵🇱"),
+
+  /// Portugal
+  portugal._("Portugal", "PT", "+351", "🇵🇹"),
+
+  /// Puerto Rico
+  puertoRico._("Puerto Rico", "PR", "+1787", "🇵🇷"),
+
+  /// Puerto Rico
+  puertoRico1939._("Puerto Rico", "PR", "+1939", "🇵🇷"),
+
+  /// Qatar
+  qatar._("Qatar", "QA", "+974", "🇶🇦"),
+
+  /// Republic of the Congo
+  republicOfTheCongo._("Republic of the Congo", "CG", "+242", "🇨🇬"),
+
+  /// Réunion
+  reunion._("Réunion", "RE", "+262", "🇷🇪"),
+
+  /// Romania
+  romania._("Romania", "RO", "+40", "🇷🇴"),
+
+  /// Russia
+  russia._("Russia", "RU", "+7", "🇷🇺"),
+
+  /// Rwanda
+  rwanda._("Rwanda", "RW", "+250", "🇷🇼"),
+
+  /// Saint Barthélemy
+  saintBarthelemy._("Saint Barthélemy", "BL", "+590", "🇧🇱"),
+
+  /// Saint Helena
+  saintHelena._("Saint Helena", "SH", "+290", "🇸🇭"),
+
+  /// Saint Helena
+  saintHelena247._("Saint Helena", "SH", "+247", "🇸🇭"),
+
+  /// Saint Kitts and Nevis
+  saintKittsAndNevis._("Saint Kitts and Nevis", "KN", "+1869", "🇰🇳"),
+
+  /// Saint Lucia
+  saintLucia._("Saint Lucia", "LC", "+1758", "🇱🇨"),
+
+  /// Saint Martin
+  saintMartin._("Saint Martin", "MF", "+590", "🇲🇫"),
+
+  /// Saint Pierre and Miquelon
+  saintPierreAndMiquelon._("Saint Pierre and Miquelon", "PM", "+508", "🇵🇲"),
+
+  /// Saint Vincent and the Grenadines
+  saintVincentAndTheGrenadines._(
+      "Saint Vincent and the Grenadines", "VC", "+1784", "🇻🇨"),
+
+  /// Samoa
+  samoa._("Samoa", "WS", "+685", "🇼🇸"),
+
+  /// San Marino
+  sanMarino._("San Marino", "SM", "+378", "🇸🇲"),
+
+  /// São Tomé and Príncipe
+  saoTomeAndPrincipe._("São Tomé and Príncipe", "ST", "+239", "🇸🇹"),
+
+  /// Saudi Arabia
+  saudiArabia._("Saudi Arabia", "SA", "+966", "🇸🇦"),
+
+  /// Senegal
+  senegal._("Senegal", "SN", "+221", "🇸🇳"),
+
+  /// Serbia
+  serbia._("Serbia", "RS", "+381", "🇷🇸"),
+
+  /// Seychelles
+  seychelles._("Seychelles", "SC", "+248", "🇸🇨"),
+
+  /// Sierra Leone
+  sierraLeone._("Sierra Leone", "SL", "+232", "🇸🇱"),
+
+  /// Singapore
+  singapore._("Singapore", "SG", "+65", "🇸🇬"),
+
+  /// Sint Maarten
+  sintMaarten._("Sint Maarten", "SX", "+1721", "🇸🇽"),
+
+  /// Slovakia
+  slovakia._("Slovakia", "SK", "+421", "🇸🇰"),
+
+  /// Slovenia
+  slovenia._("Slovenia", "SI", "+386", "🇸🇮"),
+
+  /// Solomon Islands
+  solomonIslands._("Solomon Islands", "SB", "+677", "🇸🇧"),
+
+  /// Somalia
+  somalia._("Somalia", "SO", "+252", "🇸🇴"),
+
+  /// South Africa
+  southAfrica._("South Africa", "ZA", "+27", "🇿🇦"),
+
+  /// South Georgia and the South Sandwich Islands
+  southGeorgiaAndTheSouthSandwichIslands._(
+      "South Georgia and the South Sandwich Islands", "GS", "+500", "🇬🇸"),
+
+  /// South Korea
+  southKorea._("South Korea", "KR", "+82", "🇰🇷"),
+
+  /// South Sudan
+  southSudan._("South Sudan", "SS", "+211", "🇸🇸"),
+
+  /// Spain
+  spain._("Spain", "ES", "+34", "🇪🇸"),
+
+  /// Sri Lanka
+  sriLanka._("Sri Lanka", "LK", "+94", "🇱🇰"),
+
+  /// Sudan
+  sudan._("Sudan", "SD", "+249", "🇸🇩"),
+
+  /// Suriname
+  suriname._("Suriname", "SR", "+597", "🇸🇷"),
+
+  /// Svalbard and Jan Mayen
+  svalbardAndJanMayen._("Svalbard and Jan Mayen", "SJ", "+47", "🇸🇯"),
+
+  /// Swaziland
+  swaziland._("Swaziland", "SZ", "+268", "🇸🇿"),
+
+  /// Sweden
+  sweden._("Sweden", "SE", "+46", "🇸🇪"),
+
+  /// Switzerland
+  switzerland._("Switzerland", "CH", "+41", "🇨🇭"),
+
+  /// Syria
+  syria._("Syria", "SY", "+963", "🇸🇾"),
+
+  /// Taiwan
+  taiwan._("Taiwan", "TW", "+886", "🇹🇼"),
+
+  /// Tajikistan
+  tajikistan._("Tajikistan", "TJ", "+992", "🇹🇯"),
+
+  /// Tanzania
+  tanzania._("Tanzania", "TZ", "+255", "🇹🇿"),
+
+  /// Thailand
+  thailand._("Thailand", "TH", "+66", "🇹🇭"),
+
+  /// Timor-Leste
+  timorLeste._("Timor-Leste", "TL", "+670", "🇹🇱"),
+
+  /// Togo
+  togo._("Togo", "TG", "+228", "🇹🇬"),
+
+  /// Tokelau
+  tokelau._("Tokelau", "TK", "+690", "🇹🇰"),
+
+  /// Tonga
+  tonga._("Tonga", "TO", "+676", "🇹🇴"),
+
+  /// Trinidad and Tobago
+  trinidadAndTobago._("Trinidad and Tobago", "TT", "+1868", "🇹🇹"),
+
+  /// Tunisia
+  tunisia._("Tunisia", "TN", "+216", "🇹🇳"),
+
+  /// Turkey
+  turkey._("Turkey", "TR", "+90", "🇹🇷"),
+
+  /// Turkmenistan
+  turkmenistan._("Turkmenistan", "TM", "+993", "🇹🇲"),
+
+  /// Turks and Caicos Islands
+  turksAndCaicosIslands._("Turks and Caicos Islands", "TC", "+1649", "🇹🇨"),
+
+  /// Tuvalu
+  tuvalu._("Tuvalu", "TV", "+688", "🇹🇻"),
+
+  /// Uganda
+  uganda._("Uganda", "UG", "+256", "🇺🇬"),
+
+  /// Ukraine
+  ukraine._("Ukraine", "UA", "+380", "🇺🇦"),
+
+  /// United Arab Emirates
+  unitedArabEmirates._("United Arab Emirates", "AE", "+971", "🇦🇪"),
+
+  /// United Kingdom
+  unitedKingdom._("United Kingdom", "GB", "+44", "🇬🇧"),
+
+  /// United States
+  unitedStates._("United States", "US", "+1", "🇺🇸"),
+
+  /// Uruguay
+  uruguay._("Uruguay", "UY", "+598", "🇺🇾"),
+
+  /// Uzbekistan
+  uzbekistan._("Uzbekistan", "UZ", "+998", "🇺🇿"),
+
+  /// Vanuatu
+  vanuatu._("Vanuatu", "VU", "+678", "🇻🇺"),
+
+  /// Venezuela
+  venezuela._("Venezuela", "VE", "+58", "🇻🇪"),
+
+  /// Vietnam
+  vietnam._("Vietnam", "VN", "+84", "🇻🇳"),
+
+  /// Virgin Islands, U.S.
+  virginIslandsUS._("Virgn Islands, U.S.", "VI", "+1340", "🇻🇮"),
+
+  /// Wallis and Futuna
+  wallisAndFutuna._("Wallis and Futuna", "WF", "+681", "🇼🇫"),
+
+  /// Yemen
+  yemen._("Yemen", "YE", "+967", "🇾🇪"),
+
+  /// Zambia
+  zambia._("Zambia", "ZM", "+260", "🇿🇲"),
+
+  /// Zimbabwe
+  zimbabwe._("Zimbabwe", "ZW", "+263", "🇿🇼"),
+
+  /// Åland Islands
+  alandIslands._("Åland Islands", "AX", "+358", "🇦🇽"),
+  ;
+
+  /// Name of the country
   final String name;
 
-  /// The code of the country
+  /// This is two letter code of the country
   ///
-  /// This will be something like `US` for the United States, `GB` for the United Kingdom, `FR` for France, etc.
+  /// ISO 3166-1 alpha-2 code
+  ///
+  /// Example: US, IN, GB, DE, FR, CN, JP, RU, BR, etc.
   final String code;
 
-  /// The dial code of the country
+  /// International dial code
   final String dialCode;
 
-  /// The flag of the country
-  ///
-  /// This will be the emoji like `🇺🇸` for the United States, `🇬🇧` for the United Kingdom, `🇫🇷` for France, etc.
+  /// Flag of the country
   final String flag;
 
-  /// Creates a new Country instance
-  const Country({
-    required this.name,
-    required this.code,
-    required this.dialCode,
-    required this.flag,
-  });
+  /// Internal constructor
+  const Country._(this.name, this.code, this.dialCode, this.flag);
 
-  /// The equality operator
+  /// Returns the country name
   @override
-  bool operator ==(Object? other) =>
-      identical(this, other) ||
-      other is Country &&
-          runtimeType == other.runtimeType &&
-          name == other.name &&
-          code == other.code &&
-          dialCode == other.dialCode &&
-          flag == other.flag;
-
-  /// The hash code
-  @override
-  int get hashCode =>
-      name.hashCode ^ code.hashCode ^ dialCode.hashCode ^ flag.hashCode;
-
-  @override
-  String toString() {
-    return 'Country{name: $name, code: $code, dialCode: $dialCode, flag: $flag}';
-  }
+  String toString() => name;
 
   /// Returns a string that can be used to search for this country
   String toSearchString() {
